0e6b28e790caca548c110a940a48f204d5629e3b
kosta4310 <realanka77@gmail.com>
2022-08-16 10:14:20 +0300

    init: start develop

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-RSLang
+# RSLang
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './style.scss';
+
+console.log('hello');
+document.body.innerHTML = 
+    `<img src="./assets/images/GitHub-Mark/PNG/GitHub-Mark-32px.png" alt="logo">`;
