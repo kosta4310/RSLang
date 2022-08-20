c18deca7cd0f6b760358243ea047bb4d874fa9ea
Kostiantyn Pavlov <realanka77@gmail.com>
2022-08-20 16:12:44 +0300

    feat: add footer

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import './style.scss';
 import './components/first-page/first-page.component';
 import './components/header/header.component';
 import { Header } from './components/header/header.component';
+import { Footer } from './components/footer/footer.component';
 
 const header = new Header();
 header.init();
+
+const footer = new Footer();
+footer.init();
 
 const router = new Router();
 router.init();
