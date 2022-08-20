0d8b3aecf637a70a8333cf26deb26d0bf903af90
Kostiantyn Pavlov <realanka77@gmail.com>
2022-08-20 12:52:48 +0300

    refactor: remake to classes

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,29 +1,43 @@
 import { header } from './header.template';
 
-document.body.innerHTML = header;
+export class Header {
+    burger: HTMLElement | undefined;
+    menu: HTMLElement | undefined;
+    links: Array<HTMLElement> | undefined;
 
-const burger = <HTMLElement>document.querySelector('#burger');
-const menu = <HTMLElement>document.querySelector('#menu');
-const links = <Array<HTMLElement>>Array.from(document.querySelectorAll('#menu a'));
-
-burger.addEventListener('click', (e) => {
-    e.stopPropagation();
-    openCloseMenu();
-});
-
-document.addEventListener('click', () => {
-    if (menu.classList.contains('open')) openCloseMenu();
-});
-
-links.map((link) => link.addEventListener('click', () => openCloseMenu()));
-
-export function openCloseMenu() {
-    if (menu.classList.contains('open')) {
-        burger.style.transform = 'rotate(0deg)';
-        menu.style.left = '-320px';
-    } else {
-        burger.style.transform = 'rotate(90deg)';
-        menu.style.left = '0';
+    init(): void {
+        this.render();
+        this.burger = <HTMLElement>document.querySelector('#burger');
+        this.menu = <HTMLElement>document.querySelector('#menu');
+        this.links = <Array<HTMLElement>>Array.from(document.querySelectorAll('#menu a'));
+        this.listener();
     }
-    menu.classList.toggle('open');
+
+    render() {
+        document.body.innerHTML = header;
+    }
+
+    listener() {
+        (<HTMLElement>this.burger).addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.openCloseMenu();
+        });
+
+        document.addEventListener('click', () => {
+            if ((<HTMLElement>this.menu).classList.contains('open')) this.openCloseMenu();
+        });
+
+        (<Array<HTMLElement>>this.links).map((link) => link.addEventListener('click', () => this.openCloseMenu()));
+    }
+
+    openCloseMenu() {
+        if ((<HTMLElement>this.menu).classList.contains('open')) {
+            (<HTMLElement>this.burger).style.transform = 'rotate(0deg)';
+            (<HTMLElement>this.menu).style.left = '-320px';
+        } else {
+            (<HTMLElement>this.burger).style.transform = 'rotate(90deg)';
+            (<HTMLElement>this.menu).style.left = '0';
+        }
+        (<HTMLElement>this.menu).classList.toggle('open');
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Router } from './components/Router/router';
 import './style.scss';
 import './components/first-page/first-page.component';
-import { header } from './components/header/header.template';
+// import { header } from './components/header/header.template';
 import './components/header/header.component';
+import { Header } from './components/header/header.component';
+
+const header = new Header();
+header.init();
 
 const router = new Router();
 router.init();
-console.log('init');
