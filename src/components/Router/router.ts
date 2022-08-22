aeeda2c9a678a8228a3aae6c089c19553ffece81
Kostiantyn Pavlov <realanka77@gmail.com>
2022-08-22 10:55:00 +0300

    refactor: refactor code.

--- src/components/Router/router.ts
+++ src/components/Router/router.ts
@@ -1,15 +1,19 @@
 import Navigo from 'navigo';
-
+import { HomePage } from '../first-page/homepage.component';
 
 export class Router {
     router: Navigo;
+    homePage: HomePage;
     constructor() {
+        this.homePage = new HomePage();
         this.router = new Navigo('/', { hash: true });
     }
     init() {
         this.router
             .on('/', () => {
                 console.log('start page');
+
+                this.homePage.init();
             })
             .on('/learnbook', () => {
                 console.log('learn book');
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ import { Router } from './components/Router/router';
 import { HomePage } from './components/first-page/homepage.component';
 import './style.scss';
 
-const homePage = new HomePage();
-homePage.init();
-
 const router = new Router();
 router.init();
