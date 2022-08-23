2a842c4e78dea6ad9e2145c32e0edfe15afd630e
Artem <75018018+rocks-py@users.noreply.github.com>
2022-08-23 02:22:23 -0700

    fix: add event delegation for audio playing

--- src/components/learn-book/classLearnBook.ts
+++ src/components/learn-book/classLearnBook.ts
@@ -27,28 +27,48 @@ export class Book {
     }
 
     listen() {
-        const arrSpeakers = <NodeListOf<HTMLElement>>document.body.querySelectorAll('.speaker');
-        function playAudioArray(soundTracks: Array<string>) {
-            const audio = new Audio(soundTracks[0]);
-            const audio1 = new Audio(soundTracks[1]);
-            const audio2 = new Audio(soundTracks[2]);
-            audio.play();
-            audio.addEventListener('ended', () => {
-                audio1.play();
-            });
-            audio1.addEventListener('ended', () => {
-                audio2.play();
-            });
+        let isPlayed = false;
+        let audio: HTMLAudioElement | null = null;
+        let audio1: HTMLAudioElement | null = null;
+        let audio2: HTMLAudioElement | null = null;
+        const playAudio1 = async () => {
+            await audio1?.play();
         }
-        arrSpeakers.forEach((speaker) =>
-            speaker.addEventListener('click', (e) => {
+        const playAudio2 = async () => {
+            await audio2?.play();
+            isPlayed = false;
+        }
+
+        function playAudioArray(soundTracks: Array<string>) {
+            if (isPlayed) {
+                audio?.removeEventListener('ended', playAudio1)
+                audio1?.removeEventListener('ended', playAudio2)
+                audio?.pause();
+                audio1?.pause();
+                audio2?.pause();
+            }
+            
+            audio = new Audio(soundTracks[0]);
+            audio1 = new Audio(soundTracks[1]);
+            audio2 = new Audio(soundTracks[2]);
+            isPlayed = true;
+            audio.play();
+            audio.addEventListener('ended', playAudio1);
+            audio1.addEventListener('ended', playAudio2);
+        }
+        // для оптимизации вешаем обработчик на весь блок - делегирование событий
+        // а не на каждую кнопку
+        const words = document.body.querySelector('#words')
+        words?.addEventListener('click', (e) => {
+            const target = <HTMLElement>e.target;
+            if (target.classList.contains('speaker')) {
                 const card = <HTMLElement>(<HTMLElement>e.target).closest('.card');
                 const pathAudio = `${BASE}/${card.getAttribute('data-audio')}`;
                 const pathAudioMeaning = `${BASE}/${card.getAttribute('data-audioMeaning')}`;
                 const pathAudioExample = `${BASE}/${card.getAttribute('data-audioExample')}`;
 
                 playAudioArray([pathAudio, pathAudioMeaning, pathAudioExample]);
-            })
-        );
+            }
+        })
     }
 }
