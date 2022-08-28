3b6c2ad19fcff01680bcb36ccbba72d3383122ac
Kostiantyn Pavlov <realanka77@gmail.com>
2022-08-28 14:42:54 +0300

    feat: in process

---
+++ src/components/sprint/statisticSprintGame.template.ts
@@ -0,0 +1,22 @@
+export function statisticGameSprint() {
+    return `<div class="wrapper__statistic-sprint">
+      <div class="container">
+         <div class="statistic-sprint">
+            <div class="statistic-sprint__block">
+               <h1 class="statistic-sprint__title">
+                  Рузультаты
+               </h1>
+               <div class="statistic-sprint__table">
+                  <div class="table__line">
+                     <div class="table__sound">G</div>
+                     <div class="table__word-en">dkfjksdjfksd</div>
+                     <div class="table__word-transcript">[fdsfsdf]</div>
+                     <div class="table__word-translate">роррлжолол</div>
+                     <div class="table__word-result">X</div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>`;
+}
