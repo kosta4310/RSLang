b16cd28332b971e5ae7dae54bfb95da8d6b4afa0
Artem <75018018+rocks-py@users.noreply.github.com>
2022-08-30 11:49:18 -0700

    fix [main-page]: update capabilities styles to make it look prettier

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -35,3 +35,43 @@ export const saveWord = async function(wordId: string, complexity: Difficulty = 
     console.log(result);
   }
 };
+
+// // async setStatisticWord(word: IWord, isRight: boolean) {
+// //   const wordId = word.id;
+// //   const { userId, token } = state.getItem('auth');
+// //   // let { optional } = word;
+
+// //   // const currentDate = new Date().toISOString().slice(0, 10);
+// //   const initOption = <IOptionalToWord>{
+// //       sprintCorrect: 0,
+// //       sprintTotal: 0,
+// //       audioCallCorrect: 0,
+// //       audioCallTotal: 0,
+// //       correctInLineCount: 0,
+// //   };
+
+// //   let userWord = await API.getUserWordById(userId, wordId, token);
+// //   if (typeof userWord === 'string') {
+// //       userWord = <UserWord>(
+// //           await API.createUserWord(userId, wordId, token, { difficulty: 'normal', optional: initOption })
+// //       );
+// //   }
+// //   if (isRight) {
+// //       userWord.optional.sprintCorrect += 1;
+// //       userWord.optional.correctInLineCount += 1;
+// //       if (
+// //           (userWord.difficulty === 'normal' && userWord.optional.correctInLineCount >= 3) ||
+// //           (userWord.difficulty === 'hard' && userWord.optional.correctInLineCount >= 5)
+// //       ) {
+// //           userWord.difficulty = 'easy';
+// //       }
+// //   } else userWord.optional.correctInLineCount = 0;
+
+// //   userWord.optional.sprintTotal += 1;
+// //   const noteToWord = <NoteToWord>{
+// //       difficulty: userWord.difficulty,
+// //       optional: JSON.parse(JSON.stringify(userWord.optional)),
+// //   };
+// //   const res = await API.updateUserWord(userId, wordId, token, noteToWord);
+// //   console.log(res);
+// // }
