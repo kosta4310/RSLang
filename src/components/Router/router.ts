5f4ce684f2de54d1b5202efca8d596a561d12947
Kostiantyn Pavlov <realanka77@gmail.com>
2022-09-01 10:16:26 +0300

    refactor: clean code

--- src/components/Router/router.ts
+++ src/components/Router/router.ts
@@ -25,23 +25,19 @@ export class Router {
     init() {
         this.router
             .on('/', () => {
-                console.log('start page');
                 state.isGame = false;
                 this.homePage.init();
             })
             .on('/learnbook', () => {
-                console.log('learn book');
                 state.isGame = false;
                 this.book.init();
             })
             .on('/statistic', () => {
-                console.log('statistic');
                 state.isGame = false;
             })
             .on('/audio-call', () => {
                 state.isGame = false;
                 this.audioCall.init();
-                console.log('game listen');
             })
             .on('/sprint', () => {
                 state.isGame = false;
